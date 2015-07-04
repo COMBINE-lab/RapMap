85176ce3d34753e255b233837349d467d423bc64
Rob Patro <rob.patro@gmail.com>
2015-07-04 13:38:02 -0400

    initial commit

---
+++ src/RapMap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int rapMapIndex(int argc, char* argv[]);
+
+int main(int argc, char* argv[]) {
+    if (std::string(argv[1]) == "index") {
+        return rapMapIndex(argc - 2, &argv[2]);
+    } else {
+        std::cerr << "the command " << argv[1]
+                  << " is not yet implemented\n";
+        return 1;
+    }
+    return 0;
+}
---
+++ src/RapMapIndexer.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int rapMapIndex(int argc, char* argv[]) {
+    std::cerr << "indexing\n";
+    return 0;
+}
+
+
