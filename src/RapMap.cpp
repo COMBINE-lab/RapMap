99f95efa50080d95b7c3195d19505103906b4464
Rob <robp@cs.cmu.edu>
2015-08-02 19:21:27 -0400

    Add help / usage to main program

--- src/RapMap.cpp
+++ src/RapMap.cpp
@@ -1,17 +1,50 @@
 #include <iostream>
 #include <vector>
+#include "RapMapConfig.hpp"
 
 int rapMapIndex(int argc, char* argv[]);
 int rapMapSAIndex(int argc, char* argv[]);
 int rapMapMap(int argc, char* argv[]);
 int rapMapSAMap(int argc, char* argv[]);
 
+void printUsage() {
+    std::string versionString = rapmap::version;
+    std::cerr << "RapMap Transcriptome Aligner v"
+              << versionString << '\n';
+    std::cerr << "=====================================\n";
+    auto usage =
+        R"(
+There are currently 4 RapMap subcommands
+    index   --- builds a k-mer-based index
+    map     --- map reads using a k-mer-based index
+    saindex --- builds a suffix array-based (SA) index
+    samap   --- map reads using the SA-based index
+
+Run a corresponding command "rapmap <cmd> -h" for
+more information on each of the possible RapMap
+commands.)";
+    std::cerr << usage << '\n';
+}
+
+
 int main(int argc, char* argv[]) {
 
     std::vector<char*> args;
     args.push_back(argv[0]);
+
+    if (argc < 2) {
+        printUsage();
+        std::exit(0);
+    }
+
     for (int i = 2; i < argc; ++i) {
         args.push_back(argv[i]);
+    }
+
+    if (std::string(argv[1]) == "-h" or
+        std::string(argv[1]) == "--help") {
+        printUsage();
+        std::exit(0);
     }
 
     if (std::string(argv[1]) == "index") {
