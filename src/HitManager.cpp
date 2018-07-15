1325189af2c406832b09a578db128b069e6a9f28
Rob Patro <rob@newton>
2018-07-15 11:51:11 -0400

    fastlog

--- src/HitManager.cpp
+++ src/HitManager.cpp
@@ -26,6 +26,27 @@
 
 namespace rapmap {
     namespace hit_manager {
+
+      // fastlog2 and fasterlog2 from : https://raw.githubusercontent.com/romeric/fastapprox/master/fastapprox/src/fastlog.h
+      // license information included at the bottom of this file.
+      static inline float fastlog2 (float x) {
+        union { float f; uint32_t i; } vx = { x };
+        union { uint32_t i; float f; } mx = { (vx.i & 0x007FFFFF) | 0x3f000000 };
+        float y = vx.i;
+        y *= 1.1920928955078125e-7f;
+
+        return y - 124.22551499f
+          - 1.498030302f * mx.f 
+          - 1.72587999f / (0.3520887068f + mx.f);
+      }
+
+      static inline float fasterlog2 (float x) {
+        union { float f; uint32_t i; } vx = { x };
+        float y = vx.i;
+        y *= 1.1920928955078125e-7f;
+        return y - 126.94269504f;
+      }
+
     	// Return hits from processedHits where position constraints
         // match maxDist
         bool collectHitsSimple(std::vector<ProcessedHit>& processedHits,
@@ -107,7 +128,7 @@ namespace rapmap {
                   }
                   double l = qdiff - rdiff;
                   int32_t al = std::abs(l);
-                  return (l == 0) ? 0.0 : (0.01 * avgseed * al + 0.5 * std::log2(al));
+                  return (l == 0) ? 0.0 : (0.01 * avgseed * al + 0.5 * fastlog2(static_cast<float>(al)));
                 };
 
                 double bottomScore = std::numeric_limits<double>::lowest();
@@ -817,7 +838,7 @@ d
         auto doChaining = mc.doChaining;
 
         auto fwdHitsStart = hits.size();
-        int32_t maxSlack = mc.maxSlack;//(doChaining) ? 1 : 0;
+        int32_t maxSlack = mc.maxSlack;
 
         // If we had > 1 forward hit
         if (fwdSAInts.size() > 1) {
@@ -1024,3 +1045,46 @@ d
                                                    HitCollectorInfo<SAIntervalHit<typename SAIndex64BitPerfect::IndexType>>& hcinfo, std::vector<rapmap::utils::QuasiAlignment>& hits);
 }
 }
+
+
+// LICENSE FOR fastlog code :
+/*=====================================================================*
+ *                   Copyright (C) 2011 Paul Mineiro                   *
+ * All rights reserved.                                                *
+ *                                                                     *
+ * Redistribution and use in source and binary forms, with             *
+ * or without modification, are permitted provided that the            *
+ * following conditions are met:                                       *
+ *                                                                     *
+ *     * Redistributions of source code must retain the                *
+ *     above copyright notice, this list of conditions and             *
+ *     the following disclaimer.                                       *
+ *                                                                     *
+ *     * Redistributions in binary form must reproduce the             *
+ *     above copyright notice, this list of conditions and             *
+ *     the following disclaimer in the documentation and/or            *
+ *     other materials provided with the distribution.                 *
+ *                                                                     *
+ *     * Neither the name of Paul Mineiro nor the names                *
+ *     of other contributors may be used to endorse or promote         *
+ *     products derived from this software without specific            *
+ *     prior written permission.                                       *
+ *                                                                     *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND              *
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,         *
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES               *
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE             *
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER               *
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                 *
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES            *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE           *
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR                *
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF          *
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT           *
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY              *
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE             *
+ * POSSIBILITY OF SUCH DAMAGE.                                         *
+ *                                                                     *
+ * Contact: Paul Mineiro <paul@mineiro.com>                            *
+ *=====================================================================*/
+
